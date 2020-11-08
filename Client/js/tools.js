1a8183db1cd4b6b7bf3163b782bb973d6ac11377
H4CK3R-01 <resiaknairolf@gmail.com>
2020-11-08 14:36:07 +0100

    Initial commit

---
+++ Client/js/tools.js
@@ -0,0 +1,86 @@
+function httpGetAsync(url, data, callback) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if(this.readyState === 4) {
+            callback(xmlHttp.responseText);
+        }
+    }
+
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.setRequestHeader("Authorization", "Basic " + btoa("florian" + ":" + "1234"));
+    xmlHttp.send(null);
+}
+
+function httpPostAsync(url, data, callback) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if(this.readyState === 4) {
+            callback(xmlHttp.responseText);
+        }
+    }
+
+    xmlHttp.open("POST", url, true); // true for asynchronous
+    xmlHttp.setRequestHeader("Authorization", "Basic " + btoa("florian" + ":" + "1234"));
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.send(JSON.stringify(data));
+}
+
+function httpDeleteAsync(url, data, callback) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if(this.readyState === 4) {
+            callback(xmlHttp.responseText);
+        }
+    }
+
+    xmlHttp.open("DELETE", url, true); // true for asynchronous
+    xmlHttp.setRequestHeader("Authorization", "Basic " + btoa("florian" + ":" + "1234"));
+    xmlHttp.send(null);
+}
+
+
+function findGetParameter(parameterName) {
+    let result = null, tmp = [];
+    location.search.substr(1).split("&").forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+    });
+    return result;
+}
+
+function selectHasValue(select, value) {
+    let obj = document.getElementById(select);
+
+    if (obj !== null) {
+        return (obj.innerHTML.indexOf('value="' + value + '"') > -1);
+    } else {
+        return false;
+    }
+}
+
+function setGetParameter(paramName, paramValue)
+{
+    let url = window.location.href;
+    let hash = location.hash;
+    url = url.replace(hash, '');
+    if (url.indexOf(paramName + "=") >= 0)
+    {
+        let prefix = url.substring(0, url.indexOf(paramName + "="));
+        let suffix = url.substring(url.indexOf(paramName + "="));
+        suffix = suffix.substring(suffix.indexOf("=") + 1);
+        suffix = (suffix.indexOf("&") >= 0) ? suffix.substring(suffix.indexOf("&")) : "";
+        url = prefix + paramName + "=" + paramValue + suffix;
+    }
+    else
+    {
+        if (url.indexOf("?") < 0)
+            url += "?" + paramName + "=" + paramValue;
+        else
+            url += "&" + paramName + "=" + paramValue;
+    }
+    window.history.replaceState("", document.title, url + hash);
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
