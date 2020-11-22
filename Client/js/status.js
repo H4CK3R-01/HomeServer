96c0c625eeaafac87a8b8fddea33ae7bf8864f4d
H4CK3R-01 <resiaknairolf@gmail.com>
2020-11-22 21:19:30 +0100

    Show passwords if user is logged in

--- Client/js/status.js
+++ Client/js/status.js
@@ -1,5 +1,5 @@
 $(function () {
-    httpGetAsync("http://ubuntu.fritz.box:9005/api/v1/resources/status/all", "", updateStatus);
+    httpGetAsync("http://localhost:5000/api/v1/resources/status/all", "", updateStatus);
 });
 
 
@@ -7,10 +7,15 @@ function updateStatus(data) {
     data = JSON.parse(data);
     $.each(data['data'], (key) => {
         if($("#" + key).length !== 0) {
-            if(data['data'][key]['data']['open']) {
-                $("#" + key + " > .card-header > .status").removeClass().addClass("status running").text("RUNNING")
+            if(data['data'][key]['open']) {
+                $("#" + key + " > .card-header > .status").removeClass().addClass("status running").text("RUNNING");
             } else {
-                $("#" + key + " > .card-header > .status").removeClass().addClass("status stopped").text("STOPPED")
+                $("#" + key + " > .card-header > .status").removeClass().addClass("status stopped").text("STOPPED");
+            }
+
+            if (data['data'][key].hasOwnProperty('user')) {
+                $("#" + key + " > .card-footer").empty();
+                $("#" + key + " > .card-footer").append(`<span>Benutzer: <b>` + data['data'][key]['user'] + `</b></span><br><span>Passwort: <b>` + data['data'][key]['password'] + `</b></span>`);
             }
         }
     });
--- api/status.py
+++ api/status.py
@@ -3,67 +3,69 @@ import socket
 
 from flask import jsonify, Blueprint
 
+from auth import basicAuth, verify_password
+
 status = Blueprint('status', __name__)
 
 
 @status.route('/api/v1/resources/status/ssh', methods=['GET'])
 def ssh():
-    return check_port('ubuntu.fritz.box', 22)
+    return check_port('ubuntu.fritz.box', 22, 'ssh')
 
 
 @status.route('/api/v1/resources/status/octoprint', methods=['GET'])
 def octoprint():
-    return check_port('3dprinter.fritz.box', 80)
+    return check_port('3dprinter.fritz.box', 80, 'octoprint')
 
 
 @status.route('/api/v1/resources/status/db', methods=['GET'])
 def db():
-    return check_port('ubuntu.fritz.box', 3306)
+    return check_port('ubuntu.fritz.box', 3306, 'db')
 
 
 @status.route('/api/v1/resources/status/phpmyadmin', methods=['GET'])
 def phpmyadmin():
-    return check_port('ubuntu.fritz.box', 9002)
+    return check_port('ubuntu.fritz.box', 9002, 'phpmyadmin')
 
 
 @status.route('/api/v1/resources/status/mqtt', methods=['GET'])
 def mqtt():
-    return check_port('ubuntu.fritz.box', 1883)
+    return check_port('ubuntu.fritz.box', 1883, 'mqtt')
 
 
 @status.route('/api/v1/resources/status/portainer', methods=['GET'])
 def portainer():
-    return check_port('ubuntu.fritz.box', 9000)
+    return check_port('ubuntu.fritz.box', 9000, 'portainer')
 
 
 @status.route('/api/v1/resources/status/pihole', methods=['GET'])
 def pihole():
-    return check_port('ubuntu.fritz.box', 9001)
+    return check_port('ubuntu.fritz.box', 9001, 'pihole')
 
 
 @status.route('/api/v1/resources/status/iobroker', methods=['GET'])
 def iobroker():
-    return check_port('ubuntu.fritz.box', 9003)
+    return check_port('ubuntu.fritz.box', 9003, 'iobroker')
 
 
 @status.route('/api/v1/resources/status/tvheadend', methods=['GET'])
 def tvheadend():
-    return check_port('ubuntu.fritz.box', 9981)
+    return check_port('ubuntu.fritz.box', 9981, 'tvheadend')
 
 
 @status.route('/api/v1/resources/status/raspap', methods=['GET'])
 def raspap():
-    return check_port('ubuntu.fritz.box', 9006)
+    return check_port('ubuntu.fritz.box', 9006, 'raspap')
 
 
 @status.route('/api/v1/resources/status/website', methods=['GET'])
 def website():
-    return check_port('ubuntu.fritz.box', 80)
+    return check_port('ubuntu.fritz.box', 80, 'website')
 
 
 @status.route('/api/v1/resources/status/api', methods=['GET'])
 def api():
-    return check_port('ubuntu.fritz.box', 9005)
+    return check_port('ubuntu.fritz.box', 9005, 'api')
 
 
 @status.route('/api/v1/resources/status/all', methods=['GET'])
@@ -85,7 +87,12 @@ def all_services():
     return jsonify({'status': 200, 'data': data})
 
 
-def check_port(host, port):
+def check_port(host, port, name):
+    if hasattr(basicAuth.get_auth(), 'username'):
+        auth = verify_password(basicAuth.get_auth().username, basicAuth.get_auth().password)
+    else:
+        auth = False
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((host, port))
     data = {
@@ -98,4 +105,18 @@ def check_port(host, port):
     else:
         data['open'] = False
     sock.close()
-    return json.dumps({'status': 200, 'data': data})
+
+    if auth:
+        with open("list.config") as config:
+            config_data = json.load(config)
+
+        if name in config_data['credentials']:
+            data['user'] = config_data["credentials"][name]["user"]
+            data['password'] = config_data["credentials"][name]["password"]
+
+    return json.dumps(data)
+
+
+def generate_json(host, port, auth):
+    if auth:
+        return jsonify({'status': 200, 'user': 'admin', 'password': '12345678', 'data': check_port(host, port)})
