5ec0e9651856d1fceabcc4ac6ae4ee328df89d11
H4CK3R-01 <resiaknairolf@gmail.com>
2020-12-03 12:35:00 +0100

    Improved rgb controller view

--- app/static/js/controllerRGB.js
+++ app/static/js/controllerRGB.js
@@ -5,69 +5,86 @@ function Circle(x, y, r, color) {
     this.color = color;
 }
 
+let colorPicker;
 $(function () {
+    colorPicker = new iro.ColorPicker('#picker', {
+        width: 400
+    });
+    colorPicker.on('color:change', function(color) {
+        //console.log(color.hexString);
+    });
+
+    let url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
     httpGetAsync("/api/v1/resources/rgb/color", "", generateCircles);
-})
+});
+
+
 
 function generateCircles(data) {
     if (isJson(data)) {
         data = JSON.parse(data);
 
-        let canvas = document.getElementById('luefter');
-        let ctx = canvas.getContext('2d');
-        canvas.width = 400
-        canvas.height = 400
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.lineWidth = 2
+        for(let l = 0; l < 4; l++) {
+            let canvas = document.getElementById('luefter' + l);
+            let ctx = canvas.getContext('2d');
+            canvas.width = 400
+            canvas.height = 400
+            ctx.translate(canvas.width / 2, canvas.height / 2);
+            ctx.lineWidth = 2
 
-        let circles = {}
-        for (let i = 0; i < 20; i++) {
-            let r = componentToHex(data.data[i][0]);
-            let g = componentToHex(data.data[i][1]);
-            let b = componentToHex(data.data[i][2]);
-            let color = r + g + b;
-            circles[i] = new Circle(Math.cos(18 * i * (Math.PI / 180)) * 170, Math.sin(18 * i * (Math.PI / 180)) * 170, 15, "#" + color);
-        }
-
-        $.each(circles, function (key, value) {
-            ctx.fillStyle = value.color;
-            ctx.beginPath();
-            ctx.arc(value.x, value.y, value.r, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.fill();
-        });
-
-        let thumbImg = document.createElement('img');
-        thumbImg.onload = function () {
-            ctx.lineWidth = 5
-            ctx.save();
-            ctx.beginPath();
-            ctx.arc(0, 0, 130, 0, Math.PI * 2, true);
-            ctx.closePath();
-            ctx.stroke();
-
-            ctx.drawImage(thumbImg, -100, -100, 200, 200);
-        };
-
-        thumbImg.src = 'static/img/luefter.png';
-
-        canvas.addEventListener('click', function (e) {
-            let xy = getMousePos(canvas, e);
-            let x = xy.x - 200;
-            let y = xy.y - 200;
-
-            for (let i = 0; i < 20; i++) {
-                if (Math.pow(x - (Math.cos(18 * i * (Math.PI / 180)) * 170), 2) + Math.pow(y - (Math.sin(18 * i * (Math.PI / 180)) * 170), 2) < Math.pow(15, 2)) {
-                    circles[i].color = document.getElementById("c").value;
-                    ctx.fillStyle = document.getElementById("c").value
-                    ctx.beginPath();
-                    ctx.arc(Math.cos(18 * i * (Math.PI / 180)) * 170, Math.sin(18 * i * (Math.PI / 180)) * 170, 15, 0, Math.PI * 2, true);
-                    ctx.closePath();
-                    ctx.fill();
-                    httpPostAsync("/api/v1/resources/rgb/color", circles, change_color_callback)
-                }
+            let circles = {}
+            for (let i = l * 20; i < l * 20 + 20; i++) {
+                let r = componentToHex(data.data[i][0]);
+                let g = componentToHex(data.data[i][1]);
+                let b = componentToHex(data.data[i][2]);
+                let color = r + g + b;
+                circles[i] = new Circle(Math.cos(18 * i * (Math.PI / 180)) * 170, Math.sin(18 * i * (Math.PI / 180)) * 170, 15, "#" + color);
             }
-        }, false);
+
+            $.each(circles, function (key, value) {
+                ctx.fillStyle = value.color;
+                ctx.beginPath();
+                ctx.arc(value.x, value.y, value.r, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.fill();
+            });
+
+            let thumbImg = document.createElement('img');
+            thumbImg.onload = function () {
+                ctx.lineWidth = 5
+                ctx.save();
+                ctx.beginPath();
+                ctx.arc(0, 0, 130, 0, Math.PI * 2, true);
+                ctx.closePath();
+                ctx.stroke();
+
+                ctx.drawImage(thumbImg, -100, -100, 200, 200);
+            };
+
+            thumbImg.src = 'static/img/luefter.png';
+
+            canvas.addEventListener('click', function (e) {
+                let xy = getMousePos(canvas, e);
+                let x = xy.x - 200;
+                let y = xy.y - 200;
+
+                for (let i = l * 20; i < l * 20 + 20; i++) {
+                    if (Math.pow(x - (Math.cos(18 * i * (Math.PI / 180)) * 170), 2) + Math.pow(y - (Math.sin(18 * i * (Math.PI / 180)) * 170), 2) < Math.pow(15, 2)) {
+                        circles[i].color = colorPicker.colors[0].hexString;
+                        ctx.fillStyle = circles[i].color;
+                        ctx.beginPath();
+                        ctx.arc(Math.cos(18 * i * (Math.PI / 180)) * 170, Math.sin(18 * i * (Math.PI / 180)) * 170, 15, 0, Math.PI * 2, true);
+                        ctx.closePath();
+                        ctx.fill();
+                        httpPostAsync("/api/v1/resources/rgb/color", circles, change_color_callback)
+                    }
+                }
+            }, false);
+        }
     } else {
         $("#notification_area").append('<div class="alert alert-danger" role="alert">Laden der Daten nicht möglich</div>');
     }
